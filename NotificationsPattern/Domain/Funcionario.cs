829d68dcda5dd2949f8cfa6544b5e2b366b73e78
diogoschimm <diogo_ti@hotmail.com.br>
2019-10-29 22:40:52 -0400

    Projeto

---
+++ NotificationsPattern/Domain/Funcionario.cs
@@ -0,0 +1,47 @@
+﻿using Flunt.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationsPattern.Domain
+{
+    public class Funcionario : Notifiable
+    {
+        public int FuncionarioId { get; private set; }
+        public string Nome { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public string CPF { get; private set; }
+        public string RG { get; private set; }
+        public DateTime DataRegistro { get; private set; }
+
+        public Funcionario(int id, string nome, DateTime dataNasc, string cpf, string rg, DateTime dataReg)
+        {
+            this.FuncionarioId = id;
+            this.Nome = nome;
+            this.DataNascimento = dataNasc;
+            this.CPF = cpf;
+            this.RG = rg;
+            this.DataRegistro = dataReg;
+        }
+
+        public bool Validate()
+        {
+            if (string.IsNullOrEmpty(this.Nome))
+                AddNotification(new Notification("Nome", "Nome inválido, deve ser preenchido"));
+
+            if (this.DataNascimento >= DateTime.Now)
+                AddNotification(new Notification("DataNascimento", "Data de Nascimento não pode ser maior ou igual a data do Sistema"));
+
+            if (this.CPF.Length != 11)
+                AddNotification(new Notification("CPF", "CPF inválido"));
+
+            if (string.IsNullOrEmpty(this.RG))
+                AddNotification(new Notification("RG", "RG deve ser preenchido"));
+
+            if (this.DataRegistro >= DateTime.Now)
+                AddNotification(new Notification("DataRegistro", "Data de Registro não pode ser maior que a Data atual do sistema"));
+
+            return this.Notifications.Count == 0;
+        }
+    }
+}
---
+++ NotificationsPattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using NotificationsPattern.Domain;
+using System;
+
+namespace NotificationsPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Informe o CPF: ");
+            string CPF = Console.ReadLine();
+
+            Console.Write("Informe o RG: ");
+            string RG = Console.ReadLine();
+
+            int id = 0;
+            DateTime dataNasc = DateTime.Now.AddYears(-10);
+            DateTime dataReg = DateTime.Now.AddYears(-10);
+
+            Console.WriteLine("");
+
+            var funcionario = new Funcionario(id, nome, dataNasc, CPF, RG, dataReg);
+            if (funcionario.Validate())
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Funcionário Válido");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("***** Funcionário Inválido ***** ");
+                foreach (var item in funcionario.Notifications)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("===> " + item.Message);
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+
+    }
+}
